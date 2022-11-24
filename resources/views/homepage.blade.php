2be6a8d7bf5f4961ff90fb98f1260a0d87494616
tiaradewi31 <tiaradewiibnatus2100686@upi.edu>
2022-11-24 21:06:08 +0700

    Berilmu

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         body{
-            background-image: ;
+            background-image: https://github.com/tiaradewi31/Berilmu/blob/main/public/img/bgberilmu.jpeg?raw=true;
         }
     </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
