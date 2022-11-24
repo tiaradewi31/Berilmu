2868a0bff4be0f9a23efd339e54aef64ae8e1799
tiaradewi31 <tiaradewiibnatus2100686@upi.edu>
2022-11-24 20:43:52 +0700

    Berilmu

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        body{
+            background-image: ;
+        }
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>BERILMU | Homepage</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg" style= "background-color : rgb(136, 212, 255)">
+    <div class="container">
+        <a class="navbar-brand" href="#">LOGO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="\login">LOG IN</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="\register">REGISTER</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    </nav>
+
+    <div class = "container text-center" style = "margin: 100px">
+        <h1>BERILMU<h1>
+        <h5>Berbagi perangkat pembelajaran dari sekolah negeri dan swasta di Indonesia<h5>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
