5a305139a2d980bad0bf248101d44d55e888f5c6
fitrifasol <fitri02farida@gmail.com>
2022-11-25 18:24:06 +0700

    Fitri 25 november

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,14 +6,13 @@
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
-  <body style="background-color: #88D4FF; position: relative; width: 1511px; height: 982px;">
-    <div class="card-group" style="position: absolute; width: 1267px; height: 718px;  left: 122px; top: 152px; background: #FFFFFF; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 42px;">
+  <body style="background-color: #88D4FF; position: relative; width: 1200px; height: 500px;">
+    <div class="card-group" style="position: absolute; width: 1150px; height: 500px;  left: 100px; top: 100px; background: #FFFFFF; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 42px;">
       <div class="card" style="background: #E8F2FD; box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25); border-radius: 42px;">
         <div class="card-body">
-          <img src="img/Logo.png" alt="Logo Berilmu" style="position: relative; width: 300px;">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <img src="img/Logo.png" alt="Logo Berilmu" style="width: 300px;">
+          <h4 style="font-family: 'Pridi'; align-items: top;">Tempat Berbagi Ilmu</h4>
+          <h4>Karena Berbagi itu Indah</h4>
         </div>
       </div>
       <div class="card">
