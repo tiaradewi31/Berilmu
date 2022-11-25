837720a4ef2d98260fdaab9a2acf81c8221147ab
reisaaulia <reisaaulias@upi.edu>
2022-11-25 14:48:45 +0700

    Reisa

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepagePage');
+    return view('homepage');
 });
 
 Route::get('/login', function () {
