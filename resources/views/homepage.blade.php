a10ff47c8d95696a9bba9057afe3a3034a2c1b7b
reisaaulia <reisaaulias@upi.edu>
2022-11-25 14:33:12 +0700

    Reisa

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,7 +10,7 @@
         }
     </style>    
     <h1>BERILMU<h1>
-        <h5>Berbagi perangkat pembelajaran dari sekolah Negeri dan Swasta di Indonesia<h5>
+        <h5>Berbagi perangkat pembelajaran dari sekolah Negeri dan Swasta di Indonesia yeay<h5>
         <form class="d-flex">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
