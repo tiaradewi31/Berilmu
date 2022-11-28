7e915141c7ab7049c4e4d57757305d5264a8998e
tiaradewi31 <tiaradewiibnatus2100686@upi.edu>
2022-11-28 20:01:59 +0700

    tiara

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,7 +10,12 @@
 
     <div class ="container text-center">
         <img src="img/togaberilmu.png" alt="" style = "width: 688px; height: 182px; margin: 227px;">
-
+        <div class="card text-center" style="background-color: #46B8F9;">
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
