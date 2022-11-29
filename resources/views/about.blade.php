f1ec6c640c193c4a4ba2fb9cb98e544c4714aad2
tiaradewi31 <tiaradewiibnatus2100686@upi.edu>
2022-11-29 19:48:54 +0700

    Tiara

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,13 +9,30 @@
   <body>
 
     <div class ="container text-center">
-        <img src="img/togaberilmu.png" alt="" style = "width: 688px; height: 182px; margin: auto;">
-        <div class="card text-center" style="background-color: #E4F5FF;">
+        <img src="img/togaberilmu.png" alt="" style = "width: 688px; height: 182px; margin: 176px;">
+        <div class="card" style="background-color: #E4F5FF; border-radius: 60px;">
             <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laboriosam voluptatem dicta debitis voluptatibus? Temporibus molestiae sequi culpa, quasi modi sapiente iusto, quam minus placeat, quos consequatur delectus cumque. Repellendus? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis modi molestiae voluptatum architecto, blanditiis rem doloremque deserunt praesentium recusandae porro in, quo velit laborum? Suscipit neque aliquam blanditiis vel quod! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nulla dolorem officiis aperiam repellendus reiciendis provident porro non asperiores sequi, ad soluta. Ducimus est error magni quod, laboriosam dolorem explicabo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nobis quis rerum quasi consequatur vel eum maxime molestias, cupiditate sunt praesentium asperiores harum. Recusandae odit qui ducimus sint explicabo. Quibusdam?</p>
             </div>
         </div>
+    </div>
+
+    <div class="container text-center">
+      <h2 style ="margin:80px;">Our Team</h2>
+      <div class="row">
+        <div class="col">
+          Column
+        </div>
+        <div class="col">
+          Column
+        </div>
+        <div class="col">
+          Column
+        </div>
+        <div class="col">
+          Column
+        </div>
+      </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
