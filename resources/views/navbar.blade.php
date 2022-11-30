48a1e9a3c3643ba20896187760d6088c9d6d3a4f
reisaaulia <reisaaulias@upi.edu>
2022-11-30 21:46:19 +0700

    reisa

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar fixed-top navbar-expand-lg" style= "background-color : rgb(136, 212, 255)">
     <div class="container">
         <a class="navbar-brand" href="/">
-            <img src="img/Toga.png"  alt="" width="60">
+            <img src="img/logoputih.png"  alt="" width="60">
         </a>    
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
