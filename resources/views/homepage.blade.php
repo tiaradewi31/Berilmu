c29754380d06fe894723ea349bda22677daccad1
fitrifasol <fitri02farida@gmail.com>
2022-11-30 23:10:49 +0700

    Fitri 30 November

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -25,7 +25,7 @@
         <h1>BERILMU<h1>
         <h5>Berbagi perangkat pembelajaran dari sekolah negeri dan swasta di Indonesia<h5>
         <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <input class="form-control me-2" href type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
