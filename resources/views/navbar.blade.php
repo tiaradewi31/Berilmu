f4460b99a8ac72a78ddec963e3f45125a6720c09
reisaaulia <reisaaulias@upi.edu>
2022-11-30 21:07:07 +0700

    Reisa (30-11-2022)

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar fixed-top navbar-expand-lg" style= "background-color : rgb(136, 212, 255)">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <img src="img/Toga.png"  alt="" width="60">
+        </a>    
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" style= "color: ">
+            <div class="navMenu">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="\login">LOG IN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="\register">REGISTER</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
