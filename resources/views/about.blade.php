4059e0fda839b4c2f8e33ff72da6decb0e2c2b6c
tiaradewi31 <tiaradewiibnatus2100686@upi.edu>
2022-11-30 20:14:53 +0700

    tiara

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,13 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BERILMU | ABOUT</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title> BERILMU | ABOUT</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" rel="icon" href="img/logotoga.png">
   </head>
   <body>
-
     <div class ="container text-center">
-        <img src="img/togaberilmu.png" alt="" style = "width: 688px; height: 182px; margin: 176px;">
+        <img src="img/togaberilmu.png" alt="" style = "width: 50%; height: 182px; margin: 176px;">
         <div class="card" style="background-color: #E4F5FF; border-radius: 60px;">
             <div class="card-body">
                 <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia laboriosam voluptatem dicta debitis voluptatibus? Temporibus molestiae sequi culpa, quasi modi sapiente iusto, quam minus placeat, quos consequatur delectus cumque. Repellendus? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis modi molestiae voluptatum architecto, blanditiis rem doloremque deserunt praesentium recusandae porro in, quo velit laborum? Suscipit neque aliquam blanditiis vel quod! Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nulla dolorem officiis aperiam repellendus reiciendis provident porro non asperiores sequi, ad soluta. Ducimus est error magni quod, laboriosam dolorem explicabo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nobis quis rerum quasi consequatur vel eum maxime molestias, cupiditate sunt praesentium asperiores harum. Recusandae odit qui ducimus sint explicabo. Quibusdam?</p>
@@ -21,13 +20,15 @@
       <h2 style ="margin:80px;">Our Team</h2>
       <div class="row">
         <div class="col">
-          Column
+          <img src="img/Fitri.png" class="rounded-circle" alt="" style="width:100%;">
+          <h5 style="heigth: 20px;">Fitri Farida Solihati</h5>
         </div>
         <div class="col">
           Column
         </div>
         <div class="col">
-          Column
+          <img src="img/Reisa.jpg" class="rounded-circle" alt="" style="width:100%;">
+          <h5 style="heigth: 20px;">Reisa Aulia Sodikin</h5>
         </div>
         <div class="col">
           Column
