18cc637bd124796938047b1cdd357f19843140f3
tiaradewi31 <tiaradewiibnatus2100686@upi.edu>
2022-11-30 22:46:57 +0700

    tiara

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -57,14 +57,14 @@
           <div class="col-12 col-sm-6 col-lg-3">
             <div class="single_advisor_profile wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
               <!-- Team Thumb-->
-              <div class="advisor_thumb"><img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="">
+              <div class="advisor_thumb"><img src="img/Fitri.png" alt="" style="width:120%;">
                 <!-- Social Info-->
-                <div class="social-info"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-linkedin"></i></a></div>
+                <div class="social-info"><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"></a></div>
               </div>
               <!-- Team Details-->
               <div class="single_advisor_details_info">
-                <h6>Samantha Sarah</h6>
-                <p class="designation">Founder &amp; CEO</p>
+                <h6>Fitri Farida Solihati</h6>
+                <p class="designation">Backend</p>
               </div>
             </div>
           </div>
