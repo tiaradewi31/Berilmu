1a6808ac0414cdaf1cab4df5a0c023366baeb8c9
tiaradewi31 <tiaradewiibnatus2100686@upi.edu>
2022-11-30 23:30:49 +0700

    tiara

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,11 +20,11 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" style= "color: ">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
           <div class="navMenu">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="\profil"><img src="img/fotoprofil.png" alt="" style="width:50%;"></a>
+                <a class="nav-link" aria-current="page" href="\profil"><img src="img/fotoprofil.png" alt="" style="width:40%;"></a>
               </li>
             </ul>
           </div>
@@ -46,8 +46,8 @@
           <div class="col-12 col-sm-8 col-lg-6">
             <!-- Section Heading-->
             <div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
-              <h3>Our Creative <span> Team</span></h3>
-              <p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
+              <h3 style="margin-top:80px;">Our Team</h3>
+              <p>Website ini merupakan projek akhir dari Kelompok 1 untuk mata kuliah Pemrograman Internet</p>
               <div class="line"></div>
             </div>
           </div>
@@ -64,6 +64,7 @@
               <!-- Team Details-->
               <div class="single_advisor_details_info">
                 <h6>Fitri Farida Solihati</h6>
+                <h9>2103753</h9>
                 <p class="designation">Backend</p>
               </div>
             </div>
@@ -79,6 +80,7 @@
               <!-- Team Details-->
               <div class="single_advisor_details_info">
                 <h6>Nikita Marsya Devana S</h6>
+                <h9>2102423</h9>
                 <p class="designation">Frontend</p>
               </div>
             </div>
@@ -94,6 +96,7 @@
               <!-- Team Details-->
               <div class="single_advisor_details_info">
                 <h6>Reisa Aulia Sodikin</h6>
+                <h9>2102423</h9>
                 <p class="designation">Frontend</p>
               </div>
             </div>
@@ -109,6 +112,7 @@
               <!-- Team Details-->
               <div class="single_advisor_details_info">
                 <h6>Tiara Dewi Ibnatun S</h6>
+                <h9>2100686</h9>
                 <p class="designation">Backend</p>
               </div>
             </div>
@@ -116,30 +120,6 @@
           </div>
         </div>
       </div>
-
-    <div class="container text-center">
-      <h2 style ="margin:80px;">Our Team</h2>
-      <div class="row">
-        <div class="col">
-          <img src="img/Fitri.png" class="rounded-circle" alt="Fitri Farida Solihati" style="width:75%;">
-          <h5 style="margin:20px;">Fitri Farida Solihati</h5>
-          <p>2103753</p>
-        </div>
-        <div class="col">
-          Column
-        </div>
-        <div class="col">
-          <img src="img/Reisa.jpg" class="rounded-circle" alt="Reisa Aulia Sodikin" style="width: 75%;">
-          <h5 style="margin:20px;">Reisa Aulia Sodikin</h5>
-          <p>2102423</p>
-        </div>
-        <div class="col">
-          <img src="img/Tiara.jpeg" class="rounded-circle" alt="Tiara Dewi Ibnatun Solihat" style="width:75%;">
-          <h5 style="margin:20px;">Tiara Dewi Ibnatun Solihat</h5>
-          <p>2100686</p>
-        </div>
-      </div>
-    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </body>
