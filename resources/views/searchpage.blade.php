be551ed8c6b94af743da64043c52b5f64f718177
reisaaulia <reisaaulias@upi.edu>
2022-12-03 22:00:22 +0700

    reisa

--- resources/views/searchpage.blade.php
+++ resources/views/searchpage.blade.php
@@ -32,12 +32,12 @@
 
     <div class="row">
         <div class="col-md-6" style= "padding-left: 550px">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-            <label class="form-check-label" for="flexCheckChecked">Kurikulum 2013</label>
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">Kurikulum 2013</label>
         </div>
         <div class="col-md-6">
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
-            <label class="form-check-label" for="flexCheckDefault">Kurikulum Merdeka Belajar</label>
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+            <label class="form-check-label" for="flexCheckChecked">Kurikulum Merdeka Belajar</label>
         </div>
     </div>
 
