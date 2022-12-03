1091219dc82df19bebfdc781f20cd57fe4890fc4
reisaaulia <reisaaulias@upi.edu>
2022-12-03 23:00:26 +0700

    reisaaa

--- resources/views/navbarfilter.blade.php
+++ resources/views/navbarfilter.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar fixed-top navbar-expand-lg" style= "background-color : rgb(136, 212, 255)">
     <div class="container">
-        <a class="navbar-brand" href="\homepage">
+        <a class="navbar-brand" href="/">
             <img src="img/logoputih.png"  alt="" width="60">
         </a>    
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,7 +9,7 @@
         <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" style= "color: ">
             <div class="navMenu">
                 <ul class="navbar-nav">
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown" style="padding-left: 20px; padding-top: 10px;">
                     <a class="nav-link dropdown-toggle" style= "color : black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Jenis</a>
                     <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="#">RPP</a></li>
@@ -19,7 +19,7 @@
                     <li><a class="dropdown-item" href="#">ATP</a></li>
                     </ul>
                 </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown" style="padding-left: 20px; padding-top: 10px;">
                     <a class="nav-link dropdown-toggle" style= "color : black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Jenjang</a>
                     <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="#">SD</a></li>
@@ -28,15 +28,15 @@
                     <li><a class="dropdown-item" href="#">SMK</a></li>
                     </ul>
                 </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown" style="padding-left: 20px; padding-top: 10px;">
                     <a class="nav-link dropdown-toggle" style= "color : black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Jenis Sekolah</a>
                     <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="#">Negeri</a></li>
                     <li><a class="dropdown-item" href="#">Swasta</a></li>
-                    <li><a class="dropdown-item" href="#">Madrasah Aliyah</a></li>
+                    <li><a class="dropdown-item" href="#">Madrasah</a></li>
                     </ul>
                 </li>
-                <li class="nav-item dropdown">
+                <li class="nav-item dropdown" style="padding-left: 20px; padding-top: 10px;">
                     <a class="nav-link dropdown-toggle" style= "color : black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Mata Pelajaran</a>
                     <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="#">PKN</a></li>
@@ -51,8 +51,10 @@
                     <li><a class="dropdown-item" href="#">Fisika</a></li>
                     <li><a class="dropdown-item" href="#">Biologi</a></li>
                     <li><a class="dropdown-item" href="#">Kimia</a></li>
-
                     </ul>
+                </li>
+                <li class="nav-item" style="padding-left: 20px">
+                    <a class="nav-link" aria-current="page" href="\profil"><img src="img/fotoprofil.png" alt="" style="width:40%;"></a>
                 </li>
                 </ul>
             </div>
