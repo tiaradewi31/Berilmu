7f3aa4c37e0d7752e026669fc5f307773671cf7b
reisaaulia <reisaaulias@upi.edu>
2022-12-03 21:12:58 +0700

    reisa

--- resources/views/navbarsudahlogin.blade.php
+++ resources/views/navbarsudahlogin.blade.php
@@ -1,18 +1,20 @@
-<nav class="navbar fixed-top navbar-expand-lg" style= "background-color : rgb(136, 212, 255)">
-    <div class="container">
-        <a class="navbar-brand" href="/">
-            <img src="img/logoputih.png"  alt="" width="60">
+    <nav class="navbar fixed-top navbar-expand-lg" style= "background-color : rgb(136, 212, 255)">
+      <div class="container">
+        <a class="navbar-brand" href="\homepage">
+          <img src="img/logoputih.png"  alt="" width="60">
         </a>    
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="container-kanan">
-        <a class="navbar-brand" href="/">
-            <img src="img/fotoprofil.png"  alt="" width="60">
-        </a>    
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    </div>
-    </div>
-</nav>
+        <div class=" justify-content-end" id="navbarSupportedContent">
+          <div class="navMenu">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="\profil"><img src="img/fotoprofil.png" alt="" style="width:40%;"></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
