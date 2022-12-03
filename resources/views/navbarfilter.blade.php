e2660cf2a745ebbd4131d7ce6fdadaf61ffca3ee
reisaaulia <reisaaulias@upi.edu>
2022-12-03 21:52:05 +0700

    reisa

---
+++ resources/views/navbarfilter.blade.php
@@ -0,0 +1,61 @@
+<nav class="navbar fixed-top navbar-expand-lg" style= "background-color : rgb(136, 212, 255)">
+    <div class="container">
+        <a class="navbar-brand" href="/">
+            <img src="img/logoputih.png"  alt="" width="60">
+        </a>    
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent" style= "color: ">
+            <div class="navMenu">
+                <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" style= "color : black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Jenis</a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">RPP</a></li>
+                    <li><a class="dropdown-item" href="#">Silabus</a></li>
+                    <li><a class="dropdown-item" href="#">Media Pembelajaran</a></li>
+                    <li><a class="dropdown-item" href="#">Modul Ajar</a></li>
+                    <li><a class="dropdown-item" href="#">ATP</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" style= "color : black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Jenjang</a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">SD</a></li>
+                    <li><a class="dropdown-item" href="#">SMP</a></li>
+                    <li><a class="dropdown-item" href="#">SMA</a></li>
+                    <li><a class="dropdown-item" href="#">SMK</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" style= "color : black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Jenis Sekolah</a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Negeri</a></li>
+                    <li><a class="dropdown-item" href="#">Swasta</a></li>
+                    <li><a class="dropdown-item" href="#">Madrasah Aliyah</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" style= "color : black" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Mata Pelajaran</a>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">PKN</a></li>
+                    <li><a class="dropdown-item" href="#">Matematika</a></li>
+                    <li><a class="dropdown-item" href="#">Bahasa Indonesia</a></li>
+                    <li><a class="dropdown-item" href="#">IPA</a></li>
+                    <li><a class="dropdown-item" href="#">IPS</a></li>
+                    <li><a class="dropdown-item" href="#">TIK</a></li>
+                    <li><a class="dropdown-item" href="#">Bahasa Inggris</a></li>
+                    <li><a class="dropdown-item" href="#">SBK</a></li>
+                    <li><a class="dropdown-item" href="#">PJOK</a></li>
+                    <li><a class="dropdown-item" href="#">Fisika</a></li>
+                    <li><a class="dropdown-item" href="#">Biologi</a></li>
+                    <li><a class="dropdown-item" href="#">Kimia</a></li>
+
+                    </ul>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/searchpage.blade.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+    </style>
+    <link rel="icon" href="http://localhost:8080/Tiara/apa/img/logotoga.png" type = "image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>BERILMU | Homepage</title>
+    </head>
+  <body>
+
+  @include('navbarfilter')
+
+    <div class="container">
+      <div class = "text-center" style="margin: 100px;">
+      <style>
+        body{
+            background-color: #E8F2FD;
+            background-size: cover;
+            padding-top: 90px;
+        }
+      </style>
+        <h3>PERANGKAT PEMBELAJARAN<h3>
+        <form class="d-flex" style="padding-top: 20px" >
+            <input class="form-control me-2" href type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6" style= "padding-left: 550px">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">Kurikulum 2013</label>
+        </div>
+        <div class="col-md-6">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+            <label class="form-check-label" for="flexCheckChecked">Kurikulum Merdeka Belajar</label>
+        </div>
+    </div>
+
+  <div class="custom_container">
+    <div class="fitur_berilmu" style="padding-top: 200px;">
+    <div class="hasilpencarian">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 200px;">
+            <img src="img/SilabusTIK.webp"  alt="" width="90%">
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 20px;padding-right: 100px;">
+            <h3>Judul</h3>
+            <h3>Nama Penulis</h3>
+            <h3>Tanggal terbit</h3>
+            <p class="">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          </div>
+        </div>
+    </div>
+    <div class="fitur_berilmu">
+    <div class="hasilpencarian">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 200px;">
+            <img src="img/SilabusTIK.webp"  alt="" width="90%">
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 20px;padding-right: 100px;">
+            <h3>Judul</h3>
+            <h3>Nama Penulis</h3>
+            <h3>Tanggal terbit</h3>
+            <p class="">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          </div>
+        </div>
+    </div>
+    <div class="fitur_berilmu">
+    <div class="hasilpencarian">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 200px;">
+            <img src="img/SilabusTIK.webp"  alt="" width="90%">
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 20px;padding-right: 100px;">
+            <h3>Judul</h3>
+            <h3>Nama Penulis</h3>
+            <h3>Tanggal terbit</h3>
+            <p class="">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          </div>
+        </div>
+    </div>
+    <div class="fitur_berilmu">
+    <div class="hasilpencarian">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 200px;">
+            <img src="img/SilabusTIK.webp"  alt="" width="90%">
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 20px;padding-right: 100px;">
+            <h3>Judul</h3>
+            <h3>Nama Penulis</h3>
+            <h3>Tanggal terbit</h3>
+            <p class="">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          </div>
+        </div>
+    </div>
+    <div class="fitur_berilmu" style="padding-bottom: 150px;">
+    <div class="hasilpencarian">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 200px;">
+            <img src="img/SilabusTIK.webp"  alt="" width="90%">
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-12" style="padding-top: 70px;padding-left: 20px;padding-right: 100px;">
+            <h3>Judul</h3>
+            <h3>Nama Penulis</h3>
+            <h3>Tanggal terbit</h3>
+            <p class="">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
+    @include('footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
