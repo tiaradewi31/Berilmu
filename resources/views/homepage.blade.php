ad5220bb033c56dbefb1b1c8836c809053a8d7a2
reisaaulia <reisaaulias@upi.edu>
2022-12-11 22:57:03 +0700

    reisaaaa

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -105,8 +105,11 @@
           </div>
         </div>
       </div>
-      <img src="img/bgberilmu.png" alt="" width="50%" style="padding: 5% 0% 0% 0%;">
+      <img src="img/bgguru.png" alt="" width="75%" style="padding: 0% 0% 0% 28%;">
     </div>
+    
+    @include ('topbutton')
+
     @include('footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
--- resources/views/homepagesudahlogin.blade.php
+++ resources/views/homepagesudahlogin.blade.php
@@ -104,7 +104,10 @@
                  </div>
             </div>
         </div>
+        <img src="img/bgguru.png" alt="" width="75%" style="padding: 0% 0% 0% 28%;">
     </div>
+
+    @include ('topbutton')
     @include('footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   </body>
