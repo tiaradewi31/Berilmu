7e2a614a0d8af1b6b75ba4be9b3226837f9cf29e
reisaaulia <reisaaulias@upi.edu>
2022-12-30 17:17:35 +0700

    reisa

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,62 +1,75 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>HTML</title>
-<style>	
-    .footer {
-        background-color : rgb(136, 212, 255);
-    }
-	.column {
-      flex: 33.33%;
-	  height: 200px;
-	  padding: 10px;
-      padding-top: 60px;
-      padding-left: 100px;
-	  margin: 5px;
-	  text-align: left;
-	}
-    .column-dua {
-      flex: 33.33%;
-	  height: 20px;
-	  padding: 10px;
-      padding-top: 40px;
-      padding-left: 160px;
-	  margin: 5px;
-    }
-	.container-footer {
-	   display: flex;
-       background-color: rgb(136, 212, 2550);
-       padding-left: 50px;
-	}	
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML</title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <style>
+        .body{
+            display: flex;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            background: rgb(136, 212, 255);
+            font-family: sans-serif;
+        }
+        .footer{
+            bottom: 0;
+            left: 0;
+            right: 0;
+            background: rgb(136, 212, 255);
+            height: auto;
+            width: 100vw;
 
-</style>
-</head>  
+            padding-top: 40px;
+            color: #fff;
+        }
+        .footer-content{
+            background: rgb(136, 212, 255);
+            width: 100vw;
+            padding: 20px;
+            padding-top: 2%;
+            padding-bottom: 40px;
+            text-align: center;
+        }
+        .footer-content p a{
+            text-decoration: none;
+            text-align: center;
+            max-width: 500px;
+            margin: 10px auto;
+            line-height: 28px;
+            font-size: 14px;
+            color: black;
+            background: rgb(136, 212, 255);
+        }
+        .footer-bottom{
+            background: rgb(136, 212, 255);
+            width: 100vw;
+            padding-bottom: 2%;
+            text-align: center;
+            
+        }
+    </style>
+</head>
+
 <body>
-    <footer class="footer">
-	    <div class="container-footer">
-	        <div class="column">
-                <p>
-                    <a href="/about" class="nav-link text-dark" style= "font-family: 'Poppins', Sans-serif; src: url(https://fonts.google.com/specimen/Poppins?query=Poppins);">About Berilmu</a>
-                </p>
-                <!-- <p>
-                    <a href="/searchpage" class="nav-link text-dark" style= "font-family: 'Poppins', Sans-serif; src: url(https://fonts.google.com/specimen/Poppins?query=Poppins);">Cari Perangkat Pembelajaran</a>
-                </p> -->
-	        </div>
-	        <div class="column-dua">
-                <div class="sosmed" style= "padding-top: 30px; padding-left: 20px;">
-                    <img src="img/gmail.png"  alt="" width="40px">
-                    <img src="img/instagram.png"  alt="" width="30px">
-                </div>
+    <footer>
+        <div class="footer-content">
+            <img src="img/togaberilmuputih.png"  alt="" width="200">
+            <p>
+                <a href="/about">About Berilmu</a>
+            </p>
+            <img src="img/gmail.png"  alt="" width="40px">
+            <img src="img/instagram.png"  alt="" width="30px">
+        </div>
+        <div class="footer-bottom">
+            <div class="text-center" style= "padding-bottom: 15px; font-family: 'Poppins', Sans-serif; src: url(https://fonts.google.com/specimen/Poppins?query=Poppins);">
+                © 2022 Copyright:
+                <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Berilmu.com</a>
             </div>
-	        <div class="column">
-                <img src="img/togaberilmuputih.png"  alt="" width="200">
-	        </div>
-	    </div>
-        <div class="text-center" style= "padding-bottom: 15px; font-family: 'Poppins', Sans-serif; src: url(https://fonts.google.com/specimen/Poppins?query=Poppins);">
-        © 2022 Copyright:
-        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">Berilmu.com</a>
         </div>
     </footer>
 </body>
